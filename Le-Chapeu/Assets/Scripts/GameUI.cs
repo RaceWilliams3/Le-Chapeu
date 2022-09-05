5078ea580a8a927482d47810c973280aa4185144
RaceWilliams3 <willr108@my.erau.edu>
2022-09-04 23:59:57 -0700

    Boutta Reboot Save my stuff

--- Le-Chapeu/Assets/Scripts/GameUI.cs
+++ Le-Chapeu/Assets/Scripts/GameUI.cs
@@ -11,7 +11,20 @@ public class GameUI : MonoBehaviour
     public PlayerUIContainer[] playerContainers;
     public TextMeshProUGUI winText;
 
-    
+    void InitializePlayerUI()
+    {
+        //loop through all containers
+        for(int x = 0; x < playerContainers.Length; ++x)
+        {
+            PlayerUIContainer container = playerContainers[x];
+
+            // only enable an modify the UI containers we need
+            if(x < PhotonNetwork.PlayerList.Length)
+            {
+
+            }
+        }
+    }
 }
 
 [System.Serializable]
